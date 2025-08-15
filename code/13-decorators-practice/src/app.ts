35fe1d7349202febf3e7e442a5b1b616ec4ad26a
rfencl <rick_fencl@hotmail.com>
2025-08-15 15:46:42 -0700

    166 using the autobind decorator
    added eruda so I could see the dev tools on the ipad.

--- code/13-decorators-practice/src/app.ts
+++ code/13-decorators-practice/src/app.ts
@@ -1,7 +1,23 @@
+
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,13 +25,39 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
 
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault(); // Prevent default form submission behavior
+        console.log(this.titleInputElement.value);
+        // const title = this.titleInputElement.value;
+        // const description = this.descriptionInputElement.value;
+        // const people = this.peopleInputElement.value;
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
+
 }
 
 const prjInput = new ProjectInput();
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     if (window.eruda) {
+//         eruda.init();
+//     }
+// });
